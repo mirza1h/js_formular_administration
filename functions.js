059ba65245456700c28f24157639b856928c447c
Mirza Hasanovic <mirza.hasanovic1@fet.ba>
2018-11-14 13:19:32 +0100

    Dynamic add button

--- functions.js
+++ functions.js
@@ -25,27 +25,53 @@ document.getElementById("defaultOpen").click();
 function createDefaultForm(){
 	var div = document.getElementById("Administration");
 	var text = document.createElement("span");
-  text.setAttribute('display','inline');
+  text.setAttribute("display","inline");
 	var node = document.createTextNode("Element 1");
 	text.appendChild(node);
 	div.appendChild(text);
 
 	var input = document.createElement("input");
-	input.setAttribute('type','text');
-	input.setAttribute('placeholder','Label 1');
-  input.style.cssText = 'margin:10px';
+	input.setAttribute("type","text");
+	input.setAttribute("placeholder","Label 1");
+  input.className = "defaultForm";
   div.appendChild(input);
 
   var select1 = document.createElement("select");
 	var select2 = document.createElement("select");
 	var option1 = new Option("Textbox","Value",false,false);
 	var option2 = new Option("Mandatory","Value",false,false);
-  select1.style.cssText = 'margin:10px';
-  select2.style.cssText = 'margin:10px';
+  select1.className = "defaultForm";
+  select2.className = "defaultForm";
 	select1.appendChild(option1);
 	select2.appendChild(option2);
 	div.appendChild(select1);
 	div.appendChild(select2);
   var breakLine = document.createElement("br");
   div.appendChild(breakLine);
+}
+var arrayOfElements = [];
+function addItem(type) {
+  window.alert(arrayOfElements.length);
+	var div = document.getElementById("Administration");
+  if(type == "text" || type == "checkbox" || type == "radio"){
+    var element = document.createElement("input");
+    element.setAttribute("type",type);
+    element.className = "defaultForm";
+    div.appendChild(element);
+    arrayOfElements.push(element);
+    return;
+  }
+  if (type == "select"){
+    var element = document.createElement("select");
+    var option = new Option("Textbox","Value",false,false);
+	  element.appendChild(option);
+    element.className = "defaultForm";
+    div.appendChild(element);
+    arrayOfElements.push(element);
+    return;
+  }
+  var element = document.createElement(type);
+  div.appendChild(element);
+  arrayOfElements.push(element);
+  element.className = "defaultForm";
 }
