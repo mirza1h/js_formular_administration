542983d2bf6b0c8aafa9db657b16fb44ef8abc78
Mirza Hasanovic <mirza.hasanovic1@fet.ba>
2018-11-16 12:53:38 +0100

    example form

--- functions.js
+++ functions.js
@@ -35,6 +35,7 @@ function searchForm(userInput) {
     createDefaultForm();
     return;
   }
+  firstDiv.innerHTML = "";
   for( var i = 0; i < array.length; i++ ) {
     firstDiv.appendChild(array[i]);
   }
@@ -49,7 +50,7 @@ function createDefaultForm() {
   text.setAttribute("display","inline");
 	var node = document.createTextNode("Element " + elementCount);
 	text.appendChild(node);
-	firstDiv.appendChild(text);
+  firstDiv.appendChild(text);
 
 	var input = document.createElement("input");
 	input.setAttribute("type","text");
@@ -81,45 +82,6 @@ function createDefaultForm() {
 var elementCount = 0;
 var map = new Map();
 var arrayOfElements = [];
-  // Create rest of the form elements based on user dropdown selection and stores them into array
-//function addItem(type) {
-//  if(type == "")
-//    return;
-//  if(type == "text" || type == "checkbox" || type == "radio") {
-//    var element = document.createElement("input");
-//    element.setAttribute("type",type);
-//    if(type == "text") {
-//      ++labelCount;
-//    }
-//    element.setAttribute("placeholder","Label " + labelCount);
-//    element.className = "defaultForm";
-//    firstDiv.appendChild(element);
-//    arrayOfElements.push(element);
-//    return;
-//  }
-//  if (type == "select") {
-//    var element = document.createElement("select");
-//    var option = new Option("Textbox","Value",false,false);
-//	  element.appendChild(option);
-//    element.className = "defaultForm";
-//    firstDiv.appendChild(element);
-//    arrayOfElements.push(element);
-//    return;
-//  }
-//  ++elementCount;
-//  var element = document.createElement(type);
-//	var text = document.createElement("span");
-//  text.setAttribute("display","inline");
-//	var node = document.createTextNode("Element " + elementCount);
-//	text.appendChild(node);
-//  firstDiv.appendChild(element);
-//  firstDiv.appendChild(text);
-//  arrayOfElements.push(element);
-//  arrayOfElements.push(text);
-//  element.className = "defaultForm";
-//  return;
-//}
-
 var dropDown = document.getElementById("existingForm");
   // Store the copied array of current form into a map
 function storeForm(userInput) {
@@ -130,9 +92,9 @@ function storeForm(userInput) {
     var clonedArray = existingArray.slice(0);
   }
   else {
-  var option = new Option(userInput);
-  dropDown.appendChild(option);
-  var clonedArray = arrayOfElements.slice(0);
+    var option = new Option(userInput);
+    dropDown.appendChild(option);
+    var clonedArray = arrayOfElements.slice(0);
   }
   arrayOfElements = [];
   map.set(userInput,clonedArray);
@@ -146,27 +108,28 @@ function getForm(selectedItem) {
   var array = map.get(selectedItem);
   div.innerHTML = "";
   for(var i = 0; i < array.length; i++) {
-    if(array[i].tagName == "INPUT"){
+    if(array[i].tagName == "INPUT") {
       var temp = document.createElement("span");
       var node = document.createTextNode(array[i].value);
       temp.className = "defaultForm";
       temp.appendChild(node);
       div.appendChild(temp);
-    } else if (array[i].tagName == "SELECT"){
+      }
+    else if (array[i].tagName == "SELECT") {
       var temp = document.createElement("input");
+      window.alert(array[i].selectedIndex);
       temp.setAttribute("type",array[i].value);
-      //window.alert(array[i].value);
-      //window.alert(array[i+1].options[array[i+1].selectedIndex].text);
-      if(array[i+1].options[array[i+1].selectedIndex].text= "Mandatory"){
+      if(array[i+1].value == "true") {
         temp.required = true;
       }
-      if(array[i+1].options[array[i+1].selectedIndex].text= "Numeric")
+      else if(array[i+1].value == "number") {
         temp.setAttribute("type","number");
-      //window.alert(array[i].required);
+      }
       ++i;
       temp.className = "defaultForm";
       div.appendChild(temp);
-    } else if (array[i].tagName == "BR"){
+    }
+    else if (array[i].tagName == "BR"){
       var temp = document.createElement("br");
       temp.className = "defaultForm";
       div.appendChild(temp);
@@ -178,37 +141,48 @@ function getForm(selectedItem) {
   // Create an example form, so the Formulars dropdown is not empty
 function exampleForm() {
 	var text1 = document.createElement("span");
-	var text2 = document.createElement("span");
-	var text3 = document.createElement("span");
-	var text4 = document.createElement("span");
-	var node1 = document.createTextNode("Element 1");
-	var node2 = document.createTextNode("Element 2");
-	var node3 = document.createTextNode("Element 3");
-	text1.appendChild(node1);
-	text2.appendChild(node2);
-	text3.appendChild(node3);
-
-	var input1 = document.createElement("input");
+  var text2 = document.createElement("span");
+  var text3 = document.createElement("span");
+  var input1 = document.createElement("input");
 	input1.setAttribute("type","text");
-	input1.setAttribute("placeholder","Label " + 1);
   input1.className = "defaultForm";
-	var input2 = document.createElement("input");
-	input2.setAttribute("type","text");
-  input2.setAttribute("placeholder","Label " + 2);
+	input1.value = "Label 1:";
+  var input2 = document.createElement("input");;
+  input2.value = "Label 2:*";
   input2.className = "defaultForm";
-
-  var checkbox = document.createElement("input");
-  checkbox.setAttribute("type","checkbox");
-  checkbox.className = "defaultForm";
-  var node4 = document.createTextNode("Example:");
-  text4.appendChild(node4);
-  text4.className = "defaultForm";
+  var input3 = document.createElement("input");;
+  input3.value = "Label 3:*";
+  input3.className = "defaultForm";
   var select1 = document.createElement("select");
-	var option1 = new Option("Textbox","Value",false,false);
+	var option1 = new Option("Textbox","input",true,true);
+	var option2 = new Option("Checkbox","checkbox",false,false);
+	var select2 = document.createElement("select");
+	var option3 = new Option("Mandatory","true",false,false);
+	var option4 = new Option("None","false",false,false);
+	var option5 = new Option("Numeric","number",true,true);
   select1.className = "defaultForm";
+  select2.className = "defaultForm";
 	select1.appendChild(option1);
-  var breakLine = document.createElement("br");
-  arrayOfElements.push(text1,input1,select1,text2,input2,breakLine,text3,text4,checkbox);
+	select1.appendChild(option2);
+	select2.appendChild(option3);
+	select2.appendChild(option4);
+	select2.appendChild(option5);
+  var select3 = document.createElement("select");
+	var select4 = document.createElement("select");
+	var option6 = new Option("Textbox","input",false,false);
+	var option7 = new Option("Checkbox","checkbox",true,true);
+	var option8 = new Option("Mandatory","true",true,true);
+	var option9 = new Option("None","false",false,false);
+	var option10 = new Option("Numeric","number",false,false);
+  select3.className = "defaultForm";
+  select4.className = "defaultForm";
+	select3.appendChild(option6);
+	select3.appendChild(option7);
+	select4.appendChild(option8);
+	select4.appendChild(option9);
+	select4.appendChild(option10);
+  var br = document.createElement("br");
+  arrayOfElements.push(text1,input1,select1,select2,br,text2,input2,select3,select4,br);
   storeForm("Example");
   return;
 }
